82eda9ca530412b3e797ab433b405f0fe37981b5
kyungin <kyungin6453@naver.com>
2026-01-08 18:04:00 +0900

    1234

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import CheckBox03Con from "./containers/CheckBox03Con";
+import Input01Con from "./containers/Input01Con";
+import Input02Con from "./containers/Input02Con";
+import QuizRadioCon from "./containers/QuizRadioCon";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <QuizRadioCon/>
+    <hr></hr>
+    <CheckBox03Con />
+    <hr></hr>
+    <Input02Con />
+      <Input01Con />
+    </>
   );
 }
 
